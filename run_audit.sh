bbf18a5242bb92e8ffc57443e2ed296e4d3f1cfb
Mark Bolwell <mark.bollyuk@gmail.com>
2026-02-20 06:42:58 +0000

    updated script
    Signed-off-by: Mark Bolwell <mark.bollyuk@gmail.com>

--- run_audit.sh
+++ run_audit.sh
@@ -11,7 +11,7 @@
 # 03 May 2022 - update for audit variables improvement added by @pavloos - https://github.com/ansible-lockdown/RHEL8-CIS-Audit/pull/29
 # 10 Jun 2022 - added format output for different type - supports json,documentation or rspecish
 # 04 Oct 2022 - Changed default content location to /opt
-# 14 Sep 2023 - Tidyup of code,
+# 14 Sep 2023 - Tidy up of code,
 #               linting (thanks to @cf-sewe)
 #               Oracle included by default if RHEL family
 #               benchmark vars moved
@@ -20,18 +20,19 @@
 # August 2024   Improve failure capture
 # January 2025  Added Suse OS discovery
 # May 2025        Added formation typos to help and fixed some typos
+# November 2025   Updates based on v2r5, Added Additional max concurrent process option
 
 # Variables in upper case tend to be able to be adjusted
 # lower case variables are discovered or built from other variables
 
 # Goss benchmark variables (these should not need changing unless new release)
 BENCHMARK=STIG # Benchmark Name aligns to the audit
-BENCHMARK_VER=v2r3
+BENCHMARK_VER=v2r6
 BENCHMARK_OS=RHEL8
 
 # Goss host Variables
 AUDIT_BIN="${AUDIT_BIN:-/usr/local/bin/goss}"  # location of the goss executable
-AUDIT_BIN_MIN_VER="0.4.4"
+AUDIT_BIN_MIN_VER="0.4.8"
 AUDIT_FILE="${AUDIT_FILE:-goss.yml}"  # the default goss file used by the audit provided by the audit configuration
 AUDIT_CONTENT_LOCATION="${AUDIT_CONTENT_LOCATION:-/opt}"  # Location of the audit configuration file as available to the OS
 
@@ -41,12 +42,13 @@ Help()
   # Display Help
   echo "Script to run the goss audit"
   echo
-  echo "Syntax: $0 [-f|-g|-o|-v|-w|-h]"
+  echo "Syntax: $0 [-f|-g|-m|-o|-v|-w|-h]"
   echo "options:"
   echo "-f     optional - change the format output (options json(default), documentation, rspecish)"
   echo "-g     optional - Add a group that the server should be grouped with (default value = ungrouped)"
+  echo "-m     optional - maximum concurrent processes (number, default 50)"
   echo "-o     optional - file to output audit data"
-  echo "-v     optional - relative path to the vars file to load (default e.g. $AUDIT_CONTENT_LOCATION/RHEL7-$BENCHMARK/vars/$BENCHMARK.yml)"
+  echo "-v     optional - relative path to the vars file to load (default e.g. $AUDIT_CONTENT_LOCATION/{OS}-$BENCHMARK/vars/$BENCHMARK.yml)"
   echo "-w     optional - Sets the system_type to workstation (Default - Server)"
   echo "-h     Print this Help."
   echo
@@ -56,10 +58,11 @@ Help()
 host_system_type=Server
 
 ## option statement
-while getopts f:g:o:v::wh option; do
+while getopts f:g:m:o:v::wh option; do
   case "${option}" in
     f ) FORMAT=${OPTARG} ;;
     g ) GROUP=${OPTARG} ;;
+    m ) MAX=${OPTARG} ;;
     o ) OUTFILE=${OPTARG} ;;
     v ) VARS_PATH=${OPTARG} ;;
     w ) host_system_type=Workstation ;;
@@ -102,10 +105,16 @@ audit_content_dir=$AUDIT_CONTENT_LOCATION/$audit_content_version
 audit_vars=vars/${BENCHMARK}.yml
 
 # Set variable for format output
-if [ -z "$FORMAT" ]; then
+if [[ -z "$FORMAT" ]]; then
   export format="json"
 else
   export format=$FORMAT
+fi
+
+if [ -z $MAX ]; then
+  export max=50
+else
+  export max="$MAX"
 fi
 
 # Set variable for auto group
@@ -205,7 +214,7 @@ echo "#############"
 echo "Audit Started"
 echo "#############"
 echo
-$AUDIT_BIN -g "$audit_content_dir/$AUDIT_FILE" --vars "$varfile_path"  --vars-inline "$audit_json_vars" v $format_output > "$audit_out"
+$AUDIT_BIN -g "$audit_content_dir/$AUDIT_FILE" --vars "$varfile_path" --vars-inline "$audit_json_vars" v --max-concurrent "$max" $format_output > "$audit_out"
 
 # create screen output
 if [ "$(grep -c Count: "$audit_out")" -ge 1 ]  || [ "$format" = junit ] || [ "$format" = tap ]; then
