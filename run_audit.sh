c2cd5bb43f2b664fec83c53e1754cf121c9a6380
Mark Bolwell <mark.bollyuk@gmail.com>
2026-05-13 16:32:09 +0100

    Upgrade benchmark version to V2R7 (01 Apr 2026)
    - Update benchmark_version to v2r7 in vars/STIG.yml
    - Update BENCHMARK_VER to v2r7 in run_audit.sh
    
    Signed-off-by: Mark Bolwell <mark.bollyuk@gmail.com>

--- run_audit.sh
+++ run_audit.sh
@@ -27,7 +27,7 @@
 
 # Goss benchmark variables (these should not need changing unless new release)
 BENCHMARK=STIG # Benchmark Name aligns to the audit
-BENCHMARK_VER=v2r6
+BENCHMARK_VER=v2r7
 BENCHMARK_OS=RHEL8
 
 # Goss host Variables
