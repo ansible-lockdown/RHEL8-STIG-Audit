a9a81a3cb92bc0da14fff8e994005a67013bc75c
Frederick Witty <frederick.witty@gotyto.com>
2026-05-22 16:43:57 -0400

    audit hygiene: rename Changelog.MD to CHANGELOG.md, rebrand CONTRIBUTING, anchor VERSION_ID grep
    Signed-off-by: Frederick Witty <frederick.witty@gotyto.com>

--- run_audit.sh
+++ run_audit.sh
@@ -99,7 +99,7 @@ else
   fi
 fi
 
-os_maj_ver="$(grep -w VERSION_ID= /etc/os-release | awk -F\" '{print $2}' | cut -d '.' -f1)"
+os_maj_ver="$(grep '^VERSION_ID=' /etc/os-release | awk -F\" '{print $2}' | cut -d '.' -f1)"
 audit_content_version=$os_vendor$os_maj_ver-$BENCHMARK-Audit
 audit_content_dir=$AUDIT_CONTENT_LOCATION/$audit_content_version
 audit_vars=vars/${BENCHMARK}.yml
